405222d435ee725877ba802874b5ccb86141bd48
mariaminiashvili <mari.iniashvili@gmail.com>
2023-05-15 02:53:02 +0400

    add style

---
+++ script.js
@@ -0,0 +1,33 @@
+const addName = document.querySelector("#add-name");
+const inputCont = document.querySelector(".input-container");
+const errorIcon = document.querySelector(".error-icon");
+const error = document.querySelector(".error");
+const email = document.querySelector(".email");
+const done = document. querySelector(".done");
+const button = document.querySelector(".button");
+
+
+
+
+// inputCont.addEventListener("click", (e)=>{
+//     e.preventDefault();
+//     inputCont.forEach(input=>{
+//         console.log(input,value);
+//     })
+// })
+
+
+
+
+
+// let mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+// function testMail( input, error){
+//     if(mailRegex.test(input,value)){
+//         console.log("this is true");
+//         input.classlist.add ("done")
+//     }else if(mailRegex.test(input,value)){
+//         console.log("this is false");
+//         error.style.displey = "flex";
+//     }
+// }
