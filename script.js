769bf78e9181e16dbf2a6794cafc5a5367d76c3c
mariaminiashvili <mari.iniashvili@gmail.com>
2023-05-15 02:58:22 +0400

    add project

--- script.js
+++ script.js
@@ -6,28 +6,3 @@ const email = document.querySelector(".email");
 const done = document. querySelector(".done");
 const button = document.querySelector(".button");
 
-
-
-
-// inputCont.addEventListener("click", (e)=>{
-//     e.preventDefault();
-//     inputCont.forEach(input=>{
-//         console.log(input,value);
-//     })
-// })
-
-
-
-
-
-// let mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-// function testMail( input, error){
-//     if(mailRegex.test(input,value)){
-//         console.log("this is true");
-//         input.classlist.add ("done")
-//     }else if(mailRegex.test(input,value)){
-//         console.log("this is false");
-//         error.style.displey = "flex";
-//     }
-// }
